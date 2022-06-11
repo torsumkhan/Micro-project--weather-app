cbfbfbedd94c5fbfc18f4a1ef4d7ea7cf69ad7a2
torsumkhan <torsum.khan@gmail.com>
2022-06-12 01:08:37 +0500

    complete api fetch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
+import React, { useState } from "react";
 import "./index.css";
 
 function App() {
-  const api = {
-    key: "e780e668eed14efab1c8655e61adb70c",
-    base: "http://api.openweathermap.org/data/2.5/",
-  };
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e780e668eed14efab1c8655e61adb70c&units=metric`;
+
+  async function fetchWeather() {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+    setWeatherData(json);
+  }
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weatherData.main != "undefined"
+          ? weatherData.weather[0].main === "Clear"
+            ? "app clear"
+            : weatherData.weather[0].main === "Clouds"
+            ? "app clouds"
+            : weatherData.weather[0].main === "Snow"
+            ? "app snow"
+            : weatherData.weather[0].main === "Rain"
+            ? "app rain"
+            : weatherData.weather[0].main === "Fog"
+            ? "app fog"
+            : weatherData.weather[0].main === "Thunderstorm"
+            ? "app thunderstorm"
+            : weatherData.weather[0].main === "Drizzle"
+            ? "app drizzle"
+            : weatherData.weather[0].main === "Haze"
+            ? "app haze"
+            : weatherData.weather[0].main === "Mist"
+            ? "app mist"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search">
           <input
             type="text"
             className="search-bar"
-            placeholder="Enter location"
+            placeholder="Enter City"
+            onKeyPress={fetchWeather}
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
           />
         </div>
-        <div className="location-box">
-          <div className="location">Islamabad, PK</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">41°c</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weatherData.main === "undefined" ? (
+          <div>
+            <p>Welcome to weather app!</p>
+            <p>Enter your city name to see today's weather</p>
+          </div>
+        ) : (
+          <div>
+            <div className="location-box">
+              <div className="location">{location}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weatherData.main.temp)}°c</div>
+              <div className="weather">
+                {weatherData.weather[0].description}
+              </div>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
